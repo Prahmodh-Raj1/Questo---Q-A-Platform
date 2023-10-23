f4e8ee39fb1b634745085b397604c5ee183f31d1
Prahmodh R <89332177+Prahmodh-Raj1@users.noreply.github.com>
2023-10-23 22:39:18 +0530

    Add files via upload

--- server/routers/Question.js
+++ server/routers/Question.js
@@ -24,25 +24,25 @@ router.post("/", async (req, res) => {
     });
 });
 
-// router.get("/", async (req, res) => {
-//   const questions = await QuestionDB.find({});
+ /*router.get("/", async (req, res) => {
+   const questions = await QuestionDB.find({});
 
-//   try {
-//     if (questions) {
-//       res.status(200).send({ questions });
-//     } else {
-//       res.status(400).send({
-//         message: "question not found",
-//       });
-//     }
-//   } catch (e) {
-//     res.status(400).send({
-//       message: "Error in getting question",
-//     });
-//   }
-// });
-
-router.get("/:id", async (req, res) => {
+   try {
+     if (questions) {
+       res.status(200).send({ questions });
+     } else {
+       res.status(400).send({
+         message: "question not found",
+       });
+     }
+   } catch (e) {
+     res.status(400).send({
+       message: "Error in getting question",
+     });
+   }
+ });
+*/
+router.get("/id", async (req, res) => {
   try {
     // const question = await QuestionDB.findOne({ _id: req.params.id });
     // res.status(200).send(question);
@@ -52,7 +52,7 @@ router.get("/:id", async (req, res) => {
       },
       {
         $lookup: {
-          from: "Answers",
+          from: "answers",
           let: { question_id: "$_id" },
           pipeline: [
             {
@@ -78,7 +78,7 @@ router.get("/:id", async (req, res) => {
       },
       {
         $lookup: {
-          from: "Comments",
+          from: "comments",
           let: { question_id: "$_id" },
           pipeline: [
             {
@@ -119,9 +119,11 @@ router.get("/:id", async (req, res) => {
     ])
       .exec()
       .then((questionDetails) => {
+        console.log("Information has been sent")
         res.status(200).send(questionDetails);
       })
       .catch((e) => {
+        console.log("Inside the error block")
         console.log("Error: ", e);
         res.status(400).send(error);
       });
@@ -132,6 +134,97 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+/*router.get("/:id", async (req, res) => {
+  try {
+    // const question = await QuestionDB.findOne({ _id: req.params.id });
+    // res.status(200).send(question);
+    QuestionDB.aggregate([
+      {
+        $match: { _id: mongoose.Types.ObjectId(req.params.id) },
+      },
+      {
+        $lookup: {
+          from: "answers",
+          let: { question_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$question_id", "$$question_id"],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                user: 1,
+                answer: 1,
+                // created_at: 1,
+                question_id: 1,
+                created_at: 1,
+              },
+            },
+          ],
+          as: "answerDetails",
+        },
+      },
+      {
+        $lookup: {
+          from: "comments",
+          let: { question_id: "$_id" },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $eq: ["$question_id", "$$question_id"],
+                },
+              },
+            },
+            {
+              $project: {
+                _id: 1,
+                question_id: 1,
+                user: 1,
+                comment: 1,
+                // created_at: 1,
+                // question_id: 1,
+                created_at: 1,
+              },
+            },
+          ],
+          as: "comments",
+        },
+      },
+      // {
+      //   $unwind: {
+      //     path: "$answerDetails",
+      //     preserveNullAndEmptyArrays: true,
+      //   },
+      // },
+      {
+        $project: {
+          __v: 0,
+          // _id: "$_id",
+          // answerDetails: { $first: "$answerDetails" },
+        },
+      },
+    ])
+      .exec()
+      .then((questionDetails) => {
+        console.log("Information has been sent")
+        res.status(200).send(questionDetails);
+      })
+      .catch((e) => {
+        console.log("Inside the error block")
+        console.log("Error: ", e);
+        res.status(400).send(error);
+      });
+  } catch (err) {
+    res.status(400).send({
+      message: "Question not found",
+    });
+  }
+});*/
 router.get("/", async (req, res) => {
   const error = {
     message: "Error in retrieving questions",
